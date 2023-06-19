b20ae87d1675e352bbe74d4c793eb279a944c55a
Mikhailo Salko <slipknotmisha@gmail.com>
2023-06-19 14:56:36 +0300

    remove persisted store and reducer, add asyncThunks, initialState

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,36 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
 import contactsReducer from './contacts-slice';
 import filterReducer from './filter-slice';
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
-
-const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
-
 export const store = configureStore({
   reducer: {
-    contacts: persistedContactsReducer,
+    contacts: contactsReducer,
     filter: filterReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
