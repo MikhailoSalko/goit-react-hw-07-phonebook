e9d9e6f8846e6d2f6ba522d95e1b6f831d371697
Mikhailo Salko <slipknotmisha@gmail.com>
2023-06-20 14:30:44 +0300

    add styles for App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,45 @@
-import ContactForm from './ContactForm/ContactForm';
+import ContactForm from './ContactForm/ContactForm.jsx';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectContacts } from '../redux/selectors';
 import { useEffect } from 'react';
 import { fetchContacts } from 'redux/thunk';
+import Loader from './Loader/Loader';
+import {
+  StyledHeader,
+  StyledPhoneBookContainer,
+  StyledSectionContainer,
+  StyledTextNoContacts,
+} from './StyledApp';
 
 function App() {
-  const contacts = useSelector(selectContacts);
+  const {
+    contacts: { contacts, loading, error },
+  } = useSelector(state => state);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <div>
-        <div style={{ marginBottom: '30px', width: '350px' }}>
-          <h2 style={{ marginBottom: '10px', fontSize: '40px' }}>PhoneBook</h2>
-          <ContactForm />
-        </div>
-        <div>
-          <h3 style={{ marginBottom: '10px', fontSize: '30px' }}>Contacts</h3>
-          <Filter />
-          {contacts.length ? (
-            <ContactList />
-          ) : (
-            <p style={{ fontSize: 18, color: '#010101' }}>
-              There are no contacts in your phonebook
-            </p>
-          )}
-        </div>
-      </div>
-    </div>
+    <StyledPhoneBookContainer>
+      <StyledSectionContainer>
+        <StyledHeader>PhoneBook</StyledHeader>
+        <ContactForm />
+        <StyledHeader>Contacts</StyledHeader>
+        <Filter />
+      </StyledSectionContainer>
+      {error && <p>{error}</p>}
+      {loading && contacts.length === 0 ? (
+        <Loader />
+      ) : contacts.length > 0 ? (
+        <ContactList />
+      ) : (
+        <StyledTextNoContacts>
+          There are no contacts in your phonebook
+        </StyledTextNoContacts>
+      )}
+    </StyledPhoneBookContainer>
   );
 }
 
